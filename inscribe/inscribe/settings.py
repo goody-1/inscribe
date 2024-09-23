895f6b522f09a9d2dd99c5ddf375d4e114ff5c60
goody-1 <atundeg@gmail.com>
2024-09-23 11:04:50 +0100

    use dotenv instead of decouple

--- inscribe/inscribe/settings.py
+++ inscribe/inscribe/settings.py
@@ -12,13 +12,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
+from dotenv import load_dotenv
+
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Retrieve secret key from .env file
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Other settings...
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = os.getenv('DEBUG', default=False) == 'True'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
